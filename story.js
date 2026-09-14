0bcc49043ef0f356706021c4e75a0246134d8edc
helloworldba <wohelloworld@163.com>
2026-09-14 17:27:36 +0800

    Add files via upload

--- story.js
+++ story.js
@@ -14,3 +14,20 @@
     if (event.clientX < rect.left || event.clientX > rect.right || event.clientY < rect.top || event.clientY > rect.bottom) story.close();
   });
 })();
+
+(() => {
+ const dialog = document.getElementById('usageDeclaration');
+ document.getElementById('usageDeclarationButton').addEventListener('click', () => {
+  window.dispatchEvent(new Event('blur'));
+  dialog.showModal();
+  dialog.scrollTop = 0;
+ });
+ for (const id of ['usageDeclarationClose', 'usageDeclarationDone']) {
+  document.getElementById(id).addEventListener('click', () => dialog.close());
+ }
+ dialog.addEventListener('click', event => {
+  if (event.target !== dialog) return;
+  const rect = dialog.getBoundingClientRect();
+  if (event.clientX < rect.left || event.clientX > rect.right || event.clientY < rect.top || event.clientY > rect.bottom) dialog.close();
+ });
+})();
